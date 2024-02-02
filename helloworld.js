ee84ec306481b08c16eafa2386f44a64e429ca51
edmund80 <125154433+edmund80@users.noreply.github.com>
2024-02-02 18:24:09 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Week 2 Assignment)\n')
+   response.end('Week 2 Home Work Assignment)\n')
 }).listen(3333)
 
 // Console will print the message
